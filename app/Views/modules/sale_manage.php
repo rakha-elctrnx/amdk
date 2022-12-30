5069239f4250fe62ab8889c37f35ee5fe8446d28
hisyamabilkhoir <hisamabilkhoir99@gmail.com>
2022-12-30 07:56:04 +0700

    sales

--- app/Views/modules/sale_manage.php
+++ app/Views/modules/sale_manage.php
@@ -24,12 +24,12 @@ Kelola Penjualan
             </div>
             <div class="card-body">
                 <form action="<?= base_url("sale/edit") ?>" method="post">
-                    <input type="hidden" name="id" value="<?= $sale->id ?>">
+                    <input type="hidden" name="sale_id" value="<?= $sale->id ?>">
                     <div class="row mb-2">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Pelanggan</label>
-                                <select class="form-control" name="customer">
+                                <select class="form-control" name="customer_id" required>
                                     <?php
                                     foreach ($customers as $customer) {
                                         if ($customer->id == $sale->customer_id) {
@@ -119,9 +119,9 @@ Kelola Penjualan
                 <table class="table table-striped table-hover">
                     <thead>
                         <tr>
-                            <th class="text-center">Produk</th>
+                            <th class="text-center" width="350px">Produk</th>
                             <th class="text-center">Harga</th>
-                            <th class="text-center">Kts</th>
+                            <th class="text-center">Kts <small>(perhatikan satuan)</small></th>
                             <th class="text-center">Diskon</th>
                             <th class="text-center">Total</th>
                             <th class="text-center"></th>
@@ -135,26 +135,26 @@ Kelola Penjualan
                             <tr>
                                 <form action="<?= base_url("sale/item/edit") ?>" method="post">
                                     <td class='text-center'>
-                                        <input type="hidden" name="sale" value="<?= $sale->id ?>">
-                                        <input type="hidden" name="id" value="<?= $item->id ?>">
-                                        <input type="hidden" name="quantity_before" value="<?= $item->quantity_before ?>">
+                                        <input type="hidden" name="sale_id" value="<?= $sale->id ?>">
+                                        <input type="hidden" name="sale_item_id" value="<?= $item->id ?>">
+                                        <input type="hidden" name="product_variant_id" value="<?= $item->product_variant_id ?>">
                                         <input type="hidden" name="quantity_old" value="<?= $item->quantity ?>">
-                                        <input type="hidden" name="material" value="<?= $item->material_id ?>">
-                                        <input type="text" name="material_name" class="form-control" value="<?= $item->snapshot_material_name ?>" readonly>
+                                        <input type="hidden" name="product_id" value="<?= $item->product_id ?>">
+                                        <input type="text" name="snapshot_product_name" class="form-control" value="<?= $item->snapshot_product_name ?>" readonly>
                                     </td>
                                     <td class='text-center'>
                                         <div class="input-group">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text">Rp.</span>
                                             </div>
-                                            <input type="number" class="form-control text-right" name='price' value="<?= $item->price ?>" placeholder="Harga" min="1" required>
+                                            <input type="number" class="form-control text-right" name='price' value="<?= $item->price ?>" placeholder="Harga" min="1" readonly>
                                         </div>
                                     </td>
                                     <td class='text-center'>
                                         <div class="input-group">
-                                            <input type="number" class="form-control text-right" name='quantity' value="<?= $item->quantity ?>" placeholder="Kuantitas" min="0" required>
+                                            <input type="number" class="form-control text-right" name='quantity' value="<?= $item->quantity ?>" placeholder="Kuantitas" min="1" required>
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text"><?= $item->snapshot_material_unit ?></span>
+                                                <span class="input-group-text"><?= $item->snapshot_product_unit ?></span>
                                             </div>
                                         </div>
                                     </td>
@@ -168,8 +168,25 @@ Kelola Penjualan
                                     </td>
                                     <td class='text-right' width="15%">
                                         <?php
-                                        $thisTotal = ($item->price * $item->quantity) - (($item->price * $item->quantity) * $item->discount / 100);
-                                        $grandTotal += $thisTotal;
+
+                                        if ($item->product_variant_id == null) {
+                                            $thisTotal = ($item->price * $item->quantity) - (($item->price * $item->quantity) * $item->discount / 100);
+                                            $grandTotal += $thisTotal;
+                                        } else {
+                                            $productVariant = $db->table("product_variants");
+                                            $productVariant->join('sale_items', 'product_variants.id = sale_items.product_variant_id');
+                                            $productVariant->where(['sale_items.id' => $item->id, 'sale_items.sale_id' => $item->sale_id, 'sale_items.product_variant_id' => $item->product_variant_id]);
+                                            $productVariant->select([
+                                                'sale_items.quantity as sale_items_quantity',
+                                                'sale_items.price as sale_items_price',
+                                                'sale_items.discount as sale_items_discount',
+                                            ]);
+                                            $productVariant = $productVariant->get();
+                                            $productVariant = $productVariant->getFirstRow();
+
+                                            $thisTotal = ($productVariant->sale_items_price * $productVariant->sale_items_quantity) - (($productVariant->sale_items_price * $productVariant->sale_items_quantity) * $productVariant->sale_items_discount / 100);
+                                            $grandTotal += $thisTotal;
+                                        }
                                         ?>
                                         Rp. <?= number_format($thisTotal, 0, ",", ".") ?>
                                     </td>
@@ -177,7 +194,7 @@ Kelola Penjualan
                                         <button type="submit" class="btn btn-success" title="Simpan">
                                             <i class='fa fa-save'></i>
                                         </button>
-                                        <a onclick="return confirm('Yakin hapus <?= $item->snapshot_material_name ?>.?')" href="<?= base_url("sale/" . $sale->id . "/item/" . $item->id . "/delete") ?>" class='btn btn-danger'>
+                                        <a onclick="return confirm('Yakin hapus <?= $item->snapshot_product_name ?>?')" href="<?= base_url("sale/" . $sale->id . "/item/" . $item->id . "/delete") ?>" class='btn btn-danger'>
                                             <i class='fa fa-trash'></i>
                                         </a>
                                     </td>
@@ -189,7 +206,7 @@ Kelola Penjualan
                         <tr>
                             <form action="<?= base_url("sale/item/add") ?>" method="post">
                                 <td class='text-left'>
-                                    <input type="hidden" name="sale" value="<?= $sale->id ?>">
+                                    <input type="hidden" name="sale_id" value="<?= $sale->id ?>">
                                     <select name="product_id" class='form-control select2bs4' style="width: 100%;" id="product_id" required>
                                         <?php foreach ($products as $product) : ?>
 
@@ -197,12 +214,13 @@ Kelola Penjualan
                                             $exist_product = $db->table("sale_items");
                                             $exist_product->where("product_id", $product->id);
                                             $exist_product->where("sale_id", $sale->id);
+                                            $exist_product->where("product_variant_id", null);
                                             $exist_product = $exist_product->get();
                                             $exist_product = $exist_product->getResultObject();
 
                                             if ($exist_product == NULL) :
                                             ?>
-                                                <!-- <input type="hidden" value="<?= $product->id ?>" name="product_id"> -->
+
                                                 <option value="product-<?= $product->id ?>-variant-0"><?= $product->name; ?> (Satuan Default : <?= $product->unit; ?>)</option>
                                             <?php
                                             endif;
@@ -226,7 +244,7 @@ Kelola Penjualan
 
                                                 if ($exist_product_variant == NULL) :
                                                 ?>
-                                                    <!-- <input type="hidden" value="<?= $variant->id ?>" name="product_variant_id"> -->
+
                                                     <option value="product-<?= $product->id ?>-variant-<?= $variant->id ?>"><?= $product->name; ?> (Satuan Lainya : <?= $variant->unit; ?>)</option>
                                                 <?php
                                                 endif;
@@ -240,7 +258,7 @@ Kelola Penjualan
                                         <div class="input-group-prepend">
                                             <span class="input-group-text">Rp.</span>
                                         </div>
-                                        <input readonly type="number" class="form-control text-right" name='price' value="0" placeholder="Harga" min="1" required>
+                                        <input readonly type="text" class="form-control text-right" name='price' placeholder="auto" min="1" required>
                                     </div>
                                 </td>
                                 <td class='text-center'>
